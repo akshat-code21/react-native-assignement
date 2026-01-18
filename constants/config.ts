fcebad3e74850db53359acfdc8f370a0e80b14d3
Akshat <borncancer21@gmail.com>
2026-01-19 00:14:47 +0530

    Update API_BASE_URL in config.ts to use EXPO_PUBLIC_API_BASE_URL for better environment variable management.

--- constants/config.ts
+++ constants/config.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL = __DEV__
   ? 'http://localhost:8080/api/v1'
-  : process.env.API_BASE_URL;
+  : process.env.EXPO_PUBLIC_API_BASE_URL;
 
 export const API_ENDPOINTS = {
   LEADERBOARD: '/leaderboard',
